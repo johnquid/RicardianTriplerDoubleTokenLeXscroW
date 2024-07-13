924ce220d7829d7e3119e7d67fe4b24e2bf345a6
_g4brielShapir0 <gabriel.j.shapiro@gmail.com>
2024-07-13 12:14:37 +0200

    Update and rename AgreementV1.sol to ricardian-tripler-doubleTokenLeXscroW.sol
    changes filename and makes some prelim changes for tailoring to doubleTokenLeXscroW agreement

--- registry-contracts/src/ricardian-tripler-doubleTokenLeXscroW.sol
+++ registry-contracts/src/ricardian-tripler-doubleTokenLeXscroW.sol
@@ -1,11 +1,11 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.20;
 
-import "./SafeHarborRegistry.sol";
+import "./doubleTokenLeXscroWRegistry.sol";
 import "./SignatureValidator.sol";
 
 /// @notice Contract that contains the AgreementDetails that will be deployed by the Agreement Factory.
-contract AgreementV1 {
+contract ricardian-tripler-doubleTokenLeXscroW {
     /// @notice The details of the agreement.
     AgreementDetailsV1 private details;
 
@@ -73,64 +73,30 @@ contract AgreementV1Factory is SignatureValidator {
 
 /// @notice Struct that contains the details of the agreement.
 struct AgreementDetailsV1 {
-    // The name of the protocol adopting the agreement.
-    string protocolName;
-    // The contact details (required for pre-notifying).
-    string contactDetails;
-    // The scope and recovery address by chain.
-    Chain[] chains;
-    // The terms of the agreement.
-    BountyTerms bountyTerms;
-    // IPFS hash of the actual agreement document, which confirms all terms.
-    string agreementURI;
+    // The names of the parties adopting the agreement.
+    string partyAName;
+    string partyBName;
+    // The contact details of each party (required for pre-notifying).
+    string contactDetailspartyA;
+    string contactDetailspartyB;
+    // The blockchain addresses of each party.
+    string blockchainAddypartyA;
+    string blockchainAddypartyB;
+    // The assets being escrowed by each party.
+    LockedAsset lockedAssetPartyA;
+    LockedAsset lockedAssetPartyB;
+    // IPFS hash of the agreement (such as an OTC sale agreement) the LeXscroW is servicing.
+    string primaryAgreementURI;
+    // IPFS hash of the official MetaLeX LeXscroW Agreement version being agreed to which confirms all terms.
+    string LeXscroWURI;
 }
 
-/// @notice Struct that contains the details of an agreement by chain.
-struct Chain {
-    // The address to which recovered assets will be sent.
-    address assetRecoveryAddress;
-    // The accounts in scope for the agreement.
-    Account[] accounts;
-    // The chain ID.
-    uint id;
+
+
+/// @notice Struct that contains the details of a locked asset
+struct LockedAsset {
+    // details to come
 }
 
-/// @notice Enum that defines the inclusion of child contracts in an agreement.
-enum ChildContractScope {
-    // No child contracts are included
-    None,
-    // All child contracts, both existing and new, are included
-    All
-}
 
-/// @notice Struct that contains the details of an account in an agreement.
-struct Account {
-    // The address of the account (EOA or smart contract).
-    address accountAddress;
-    // The scope of child contracts included in the agreement.
-    ChildContractScope childContractScope;
-    // The signature of the account. Optionally used to verify that this account has accepted this agreement.
-    bytes signature;
-}
 
-/// @notice Whitehat identity verification methods. If Retainable, the Whitehat's
-// identity is not verified. If set to a verification service, the Whitehat's
-// identity is verified by that service. The provided services are those selected by SEAL.
-enum IdentityVerification {
-    Retainable,
-    Immunefi,
-    Bugcrowd,
-    Hackerone
-}
-
-/// @notice Struct that contains the terms of the bounty for the agreement.
-struct BountyTerms {
-    // Percentage of the recovered funds a Whitehat receives as their bounty (0-100).
-    uint bountyPercentage;
-    // The maximum bounty in USD.
-    uint bountyCapUSD;
-    // The method by which the Whitehat's identity is verified. If Retainable,
-    // the Whitehat's identity is not verified. If set to a verification service,
-    // the Whitehat's identity is verified by that service.
-    IdentityVerification verification;
-}
