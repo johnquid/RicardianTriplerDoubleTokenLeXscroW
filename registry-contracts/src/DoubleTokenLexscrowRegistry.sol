71ba8ee4896b890c91ed544bc90ce5b988c43787
Erich Dylus <51535743+ErichDylus@users.noreply.github.com>
2024-07-29 17:29:00 -0400

    Update DoubleTokenLexscrowRegistry.sol
    support party- and nonce- specific agreement registration

--- registry-contracts/src/DoubleTokenLexscrowRegistry.sol
+++ registry-contracts/src/DoubleTokenLexscrowRegistry.sol
@@ -1,13 +1,18 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.18;
 
-/// @title Double Token Lexscrow Registry
+/// @title Double Token LeXscroW Registry
+/// @notice `admin`-controlled registry contract for valid agreement factories which records duly adopted Double Token LeXscroW Agreements
+/// @dev `admin` sets valid factories with approved agreement forms
 contract DoubleTokenLexscrowRegistry {
     /// @notice admin address used to enable or disable factories.
     address public admin;
 
-    /// @notice A mapping which records the agreement details for a given governance/admin address.
-    mapping(address entity => address details) public agreements;
+    /// @notice maps an address to a counter in order to support multiple adopted agreements by one address
+    mapping(address => uint256) public nonce;
+
+    /// @notice maps an address to their index of adopted agreements to the agreement details for the applicable index
+    mapping(address adopter => mapping(uint256 index => address details)) public agreements;
 
     /// @notice A mapping which records the approved agreement factories.
     mapping(address factory => bool) public agreementFactories;
@@ -20,9 +25,9 @@ contract DoubleTokenLexscrowRegistry {
 
     /// @notice An event that records when an address either newly adopts the Double Token LeXscroW Agreement, or alters its previous terms.
     event DoubleTokenLexscrowRegistry_DoubleTokenLexscrowAdoption(
-        address indexed entity,
-        address oldDetails,
-        address newDetails
+        address confirmingParty,
+        address proposingParty,
+        address details
     );
 
     /// @notice An event that records when an address is newly enabled as a factory.
@@ -51,13 +56,23 @@ contract DoubleTokenLexscrowRegistry {
     }
 
     /// @notice Officially adopt the agreement, or modify its terms if already adopted. Only callable by approved factories.
-    /// @param details The new details of the agreement.
-    function recordAdoption(address entity, address details) external {
+    /// @dev updates mappings for each party to the agreement
+    /// @param confirmingParty address that confirmed agreement adoption
+    /// @param proposingParty address that proposed the agreement i, subsequently confirmed as adopted by `confirmingParty`
+    /// @param agreementDetailsAddress The new details of the agreement.
+    function recordAdoption(address confirmingParty, address proposingParty, address agreementDetailsAddress) external {
         if (!agreementFactories[msg.sender]) revert DoubleTokenLexscrowRegistry_OnlyFactories();
+        uint256 _confirmingPartyNonce = ++nonce[confirmingParty];
+        uint256 _proposingPartyNonce = ++nonce[proposingParty];
 
-        address oldDetails = agreements[entity];
-        agreements[entity] = details;
-        emit DoubleTokenLexscrowRegistry_DoubleTokenLexscrowAdoption(entity, oldDetails, details);
+        agreements[confirmingParty][_confirmingPartyNonce] = agreementDetailsAddress;
+        agreements[proposingParty][_proposingPartyNonce] = agreementDetailsAddress;
+
+        emit DoubleTokenLexscrowRegistry_DoubleTokenLexscrowAdoption(
+            confirmingParty,
+            proposingParty,
+            agreementDetailsAddress
+        );
     }
 
     /// @notice Enables an address as a factory.
